8f9e9c77faf3f06fe3fafe2b9b9216eec55d105a
Sulabh Saluja <sulabhsaluja111@gmail.com>
2025-07-06 00:33:19 +0530

    Time: 21 ms (20.27%), Space: 58.5 MB (46.53%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,16 +2,12 @@ import java.util.Arrays;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // Efficient sorting (uses Dual-Pivot Quicksort under the hood)
         Arrays.sort(nums);
-
-        // Check for consecutive duplicates
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] == nums[i - 1]) {
                 return true;
             }
         }
-
         return false;
     }
 }
