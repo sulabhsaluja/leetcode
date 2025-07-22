e220ae03592c29f1825a5db825e1bf9a5108bdbc
Sulabh Saluja <sulabhsaluja111@gmail.com>
2025-07-22 11:42:19 +0530

    Time: 4 ms (77.44%), Space: 43.3 MB (70.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -4,16 +4,14 @@ class Solution {
         if(sl != tl) return false;
 
         int[] arr = new int[26];
-        for(int i = 0 ; i < sl ; i++){
+        for(int i = 0 ; i < s.length() ; i++){
             arr[s.charAt(i) - 'a']++;
         }
-        for(int i = 0 ; i < sl ; i++){
+        for(int i = 0 ; i < s.length() ; i++){
             arr[t.charAt(i) - 'a']--;
-
-            if(arr[t.charAt(i) - 'a'] < 0){
-                return false;
-            }
+            if(arr[t.charAt(i) - 'a'] < 0) return false;
         }
         return true;
+
     }
 }
