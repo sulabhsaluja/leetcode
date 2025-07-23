770c06da29cbc7c260ff404d45cb9b32d2680c0b
Sulabh Saluja <sulabhsaluja111@gmail.com>
2025-07-23 13:11:12 +0530

    Time: 1 ms (99.87%), Space: 46.2 MB (46.13%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,17 +1,24 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int arrSum = 0;
+        // Set<Integer> set = new HashSet<>();
+        // int arrSum = 0;
+        // for(int num : nums){
+        //     set.add(num);
+        //     arrSum+=num;
+        // }
+        // int setSum = 0;
+        // Iterator it = set.iterator();
+        // while(it.hasNext()){
+        //     setSum+=(int)it.next();
+        // }
+        // return (2 * setSum) - arrSum;
+
+
+        int result = 0;
         for(int num : nums){
-            set.add(num);
-            arrSum+=num;
+            result = result ^ num;
         }
-        int setSum = 0;
-        Iterator it = set.iterator();
-        while(it.hasNext()){
-            setSum+=(int)it.next();
-        }
-        return (2 * setSum) - arrSum;
+        return result;
 
     }
 }
