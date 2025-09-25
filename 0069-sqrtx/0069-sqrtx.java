540b8d68ea6b566b079cd9b7ca35d9ef39eed62d
Sulabh Saluja <sulabhsaluja111@gmail.com>
2025-09-25 12:14:41 +0530

    Time: 1 ms (84.55%), Space: 41.4 MB (12.6%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,15 @@
 class Solution {
-    public int mySqrt(int x) {
-        return (int)(Math.sqrt(x));
+    public int mySqrt(int n) {
+        //the square root of any number will must be between 1 and that number itself. So,
+        int low = 1;
+        int high = n;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            long square = (long) mid * mid;
+            if(square == n) return mid;
+            else if(square > n) high = mid - 1;
+            else low = mid + 1;
+        }
+        return high;
     }
 }
