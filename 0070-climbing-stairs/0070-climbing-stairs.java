2735ef60b74c74d2c900336087c5bbb8aa94475c
Sulabh Saluja <sulabhsaluja111@gmail.com>
2025-10-06 13:55:29 +0530

    Time: 0 ms (100%), Space: 40.4 MB (51.91%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int climbStairs(int n) {
+        if (n <= 3) return n;
+        int a = 2, b = 3;
+        for (int i = 4; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
