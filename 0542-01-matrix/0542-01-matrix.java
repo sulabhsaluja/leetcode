20ff2ff9f62488b1c6ffd0c22c2d7634bf2af1cf
Sulabh Saluja <sulabhsaluja111@gmail.com>
2025-12-07 15:44:32 +0530

    Time: 15 ms (41.58%), Space: 49.4 MB (21.15%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,51 +1,45 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-        int[][] dist = new int[m][n];
+        int n = mat.length;
+        int m = mat[0].length;
+        int[][] res = new int[n][m];
+
         Queue<int[]> q = new LinkedList<>();
 
-        for(int i = 0 ; i < m ; i++){
-            for(int j = 0 ; j < n ; j++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
                 if(mat[i][j] == 0){
-                    dist[i][j] = 0;
-                    q.add(new int[]{i,j});
-                }else{
-                    dist[i][j] = -1;
+                    res[i][j] = 0;
+                    q.add(new int[]{i, j});
                 }
+                else{
+                    res[i][j] = -1;
+                }
+
             }
         }
-
         while(!q.isEmpty()){
-            int[] cell = q.poll();
-            int i = cell[0];
-            int j = cell[1];
+            int[] curr = q.poll();
+            int i = curr[0];
+            int j = curr[1];
 
-
-            //Up
-            if(i - 1 >= 0 && dist[i - 1][j] == -1){
-                dist[i-1][j] = dist[i][j] + 1;
+            if(i - 1 >= 0 && res[i - 1][j] == -1){
+                res[i - 1][j] = res[i][j] + 1;
                 q.add(new int[]{i - 1, j});
+            } 
+            if(i + 1 < n && res[i + 1][j] == -1){
+                res[i + 1][j] = res[i][j] + 1;
+                q.add(new int[]{i + 1, j});
             }
-
-            //Down
-            if(i + 1 < m && dist[i + 1][j] == -1){
-                dist[i + 1][j] = dist[i][j] + 1;
-                q.add(new int[]{i + 1,j});
-            }
-
-            //Left
-            if(j - 1 >= 0 && dist[i][j - 1] == -1){
-                dist[i][j - 1] = dist[i][j] + 1;
+            if(j - 1 >= 0 && res[i][j - 1] == -1){
+                res[i][j - 1] = res[i][j] + 1;
                 q.add(new int[]{i, j - 1});
             }
-
-            //Right
-            if(j + 1 < n && dist[i][j + 1] == -1){
-                dist[i][j + 1] = dist[i][j] + 1;
-                q.add(new int[]{i,j + 1});
+            if(j + 1 < m && res[i][j + 1] == -1){
+                res[i][j + 1] = res[i][j] + 1;
+                q.add(new int[]{i, j + 1});
             }
         }
-        return dist;
+        return res;
     }
 }
