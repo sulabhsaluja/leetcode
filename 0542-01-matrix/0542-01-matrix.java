73b4bf90f1b24dac177c47ce36d4bb7d37ebd93c
Sulabh Saluja <sulabhsaluja111@gmail.com>
2025-12-17 15:46:41 +0530

    Time: 14 ms (72.42%), Space: 49.4 MB (20.72%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,43 +1,39 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        int[][] res = new int[n][m];
-
+        int rows = mat.length;
+        int cols = mat[0].length;
+        int[][] res = new int[rows][cols];
         Queue<int[]> q = new LinkedList<>();
 
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0 ; j < m ; j++){
+        for(int i = 0 ; i < rows ; i++){
+            for(int j = 0 ; j < cols ; j++){
                 if(mat[i][j] == 0){
                     res[i][j] = 0;
-                    q.add(new int[]{i, j});
+                    q.add(new int[]{i,j});
                 }
-                else{
-                    res[i][j] = -1;
-                }
-
+                else res[i][j] = -1;
             }
         }
         while(!q.isEmpty()){
             int[] curr = q.poll();
-            int i = curr[0];
-            int j = curr[1];
+            int x = curr[0];
+            int y = curr[1];
 
-            if(i - 1 >= 0 && res[i - 1][j] == -1){
-                res[i - 1][j] = res[i][j] + 1;
-                q.add(new int[]{i - 1, j});
-            } 
-            if(i + 1 < n && res[i + 1][j] == -1){
-                res[i + 1][j] = res[i][j] + 1;
-                q.add(new int[]{i + 1, j});
+            if(x - 1 >= 0 && res[x - 1][y] == -1){
+                res[x - 1][y] = res[x][y] + 1;
+                q.add(new int[]{x - 1, y});
             }
-            if(j - 1 >= 0 && res[i][j - 1] == -1){
-                res[i][j - 1] = res[i][j] + 1;
-                q.add(new int[]{i, j - 1});
+            if(x + 1 < rows && res[x + 1][y] == -1){
+                res[x + 1][y] = res[x][y] + 1;
+                q.add(new int[]{x + 1, y});
             }
-            if(j + 1 < m && res[i][j + 1] == -1){
-                res[i][j + 1] = res[i][j] + 1;
-                q.add(new int[]{i, j + 1});
+            if(y - 1 >= 0 && res[x][y - 1] == -1){
+                res[x][y - 1] = res[x][y] + 1;
+                q.add(new int[]{x, y - 1});
+            }
+            if(y + 1 < cols && res[x][y + 1] == -1){
+                res[x][y + 1] = res[x][y] + 1;
+                q.add(new int[]{x, y + 1});
             }
         }
         return res;
