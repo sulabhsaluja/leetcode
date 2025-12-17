8666e2887eb1ef311255e277e772915f9e408cd7
Sulabh Saluja <sulabhsaluja111@gmail.com>
2025-12-17 15:20:18 +0530

    Time: 0 ms (100%), Space: 46.4 MB (71.95%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,18 +1,16 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        dfsHelper(image, sr, sc, image[sr][sc], color);
+        floodFill(image, sr, sc, image[sr][sc], color);
         return image;
     }
+    public void floodFill(int[][] img, int x, int y, int oldColor, int newColor){
+        if(x < 0 || x >= img.length || y < 0 || y >= img[0].length || img[x][y] != oldColor || img[x][y] == newColor) return;
 
-    public void dfsHelper(int[][] img, int i, int j, int origColor, int newColor){
-
-        if(i < 0 || i >= img.length || j < 0 || j >= img[0].length || img[i][j] == newColor || img[i][j] != origColor) return;
-
-        img[i][j] = newColor;
-
-        dfsHelper(img, i-1,j, origColor, newColor);
-        dfsHelper(img, i+1, j, origColor, newColor);
-        dfsHelper(img, i, j-1, origColor, newColor);
-        dfsHelper(img, i, j+1, origColor, newColor);
+        img[x][y] = newColor;
+        
+        floodFill(img, x + 1, y, oldColor, newColor);
+        floodFill(img, x - 1, y, oldColor, newColor);
+        floodFill(img, x, y + 1, oldColor, newColor);
+        floodFill(img, x, y - 1, oldColor, newColor);
     }
 }
