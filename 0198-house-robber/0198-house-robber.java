dd470cca1e1fc0628060880f579f469d88338dcd
Sulabh Saluja <sulabhsaluja111@gmail.com>
2025-12-28 10:49:35 +0530

    Time: 0 ms (100%), Space: 40.9 MB (98.92%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,9 +1,9 @@
 class Solution {
     public static int rob(int[] nums) {
-        int prev1 = 0, prev2 = 0; // prev1: last, prev2: one before last
+        int prev1 = 0, prev2 = 0; 
         for (int num : nums) {
             int temp = prev1;
-            prev1 = Math.max(prev1, prev2 + num); // choose best option at each step
+            prev1 = Math.max(prev1, prev2 + num); 
             prev2 = temp;
         }
         return prev1;
