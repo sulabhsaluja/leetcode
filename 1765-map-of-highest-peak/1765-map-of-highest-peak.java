7180d5c025f1ca1a8b516dde5dfed7c4493ce2b9
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-01-07 14:16:37 +0530

    Time: 34 ms (94.77%), Space: 184.3 MB (38.71%) - LeetHub

---
+++ 1765-map-of-highest-peak/1765-map-of-highest-peak.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length;
+        int n = isWater[0].length;
+        int[][] res = new int[m][n];
+        Queue<int[]> q = new LinkedList<>();
+        for(int i = 0 ; i < m ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(isWater[i][j] == 1){
+                    res[i][j] = 0;
+                    q.add(new int[]{i,j});
+                }
+                else res[i][j] = -1;
+            }
+        }
+        while(!q.isEmpty()){
+            int[] curr = q.poll();
+            int x = curr[0];
+            int y = curr[1];
+
+            if(x - 1 >= 0 && res[x - 1][y] == -1){
+                res[x - 1][y] = res[x][y] + 1;
+                q.add(new int[]{x - 1, y});
+            }
+            if(x + 1 < m && res[x + 1][y] == -1){
+                res[x + 1][y] = res[x][y] + 1;
+                q.add(new int[]{x + 1, y});
+            }
+            if(y - 1 >= 0 && res[x][y - 1] == -1){
+                res[x][y - 1] = res[x][y] + 1;
+                q.add(new int[]{x, y - 1});
+            }
+            if(y + 1 < n && res[x][y + 1] == -1){
+                res[x][y + 1] = res[x][y] + 1;
+                q.add(new int[]{x, y + 1});
+            }
+        }
+        return res;
+        
+    }
+}
