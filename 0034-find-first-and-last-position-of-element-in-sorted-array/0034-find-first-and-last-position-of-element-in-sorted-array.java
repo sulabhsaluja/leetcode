5cc1e50f2bb82c621d9ac2d3e98ec7c770c9a822
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-02-17 20:49:18 +0530

    Time: 0 ms (100%), Space: 48.4 MB (28.64%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,44 +1,32 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int a = firstOccurance(nums,target);
-        int b = lastOccurance(nums,target);
-        int[] arr = {a,b};
-        return arr;
+        int result[]={-1,-1};
+        int s=binarySearch(nums,target,true);
+        int e=binarySearch(nums,target,false);
+        result[0]=s;
+        result[1]=e;
+        return result;
     }
-    public int firstOccurance(int[] nums, int target){
-        int left = 0;
-        int n = nums.length;
-        int right = n - 1;
-        int idx = -1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
-                idx = mid;
-                right = mid - 1;
+    private int binarySearch(int[] nums,int target,boolean isSearchingLeft){
+        int index=-1;
+        int s=0;
+        int e=nums.length-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(nums[mid]<target){
+                s=mid+1;
+            }else if(nums[mid]>target){
+                e=mid-1;
+            }else{
+                index=mid;
+                if(isSearchingLeft){
+                    e=mid-1;
+                }else{
+                    s=mid+1;
+                }
             }
-            else if(nums[mid] < target){
-                left = mid + 1;
-            }
-            else right = mid - 1;
         }
-        return idx;
+        return index;
     }
-    public int lastOccurance(int[] nums, int target){
-        int left = 0;
-        int n = nums.length;
-        int right = n - 1;
-        int idx = -1;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(nums[mid] == target){
-                idx = mid;
-                left = mid + 1;
-            }
-            else if(nums[mid] < target){
-                left = mid + 1;
-            }
-            else right = mid - 1;
-        }
-        return idx;
-    }
+
 }
