8f6bdc3f964c040ced42eac8f31fdd370759db1f
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-03-31 11:42:23 +0530

    Time: 0 ms (100%), Space: 48.3 MB (28.83%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,15 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n< 0){
+            n = -1 * n;
+            return 1/solve(x,n);
+        }
+        return solve(x,n);
+    }
+    public double solve(double x, int n){
+        if(n == 0) return 1;
+        double half = solve(x, n / 2);
+        if(n % 2 == 0) return half * half;
+        else return half * half * x;
+    }
+}
