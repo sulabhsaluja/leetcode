2754381c0e5b7a2e9013fa18d3ac58f1074e5ffa
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-04-03 13:05:19 +0530

    Time: 2 ms (8.89%), Space: 52.9 MB (67.68%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,24 +1,13 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int n = nums.length;
-        if(nums.length == 1) return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n - 2]) return nums[n - 1];
-        int low = 0;
-        int high = nums.length - 1;
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid + 1]) return nums[mid];
-            int firstOccurance = mid;
-            int lastOccurance = mid;
-            if(nums[mid - 1] == nums[mid]) firstOccurance = mid - 1;
-            else lastOccurance = mid + 1;
-            int leftCount = firstOccurance - low;
-            int rightCount = high - lastOccurance;
-
-            if(leftCount % 2 == 0) low = lastOccurance + 1;
-            else high = firstOccurance - 1;
-        }
-        return nums[low];
+        if(n == 1) return nums[0];
+       for(int i = 1 ; i < n - 1; i++){
+        if(nums[i] > nums[i - 1] && nums[i] < nums[i + 1]) return nums[i];
+        
+       } 
+       if(nums[0] < nums[1]) return nums[0];
+       if(nums[n - 1] > nums[n - 2]) return nums[n - 1];
+       return -1;
     }
 }
