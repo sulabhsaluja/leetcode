2d5ac2c6a0491a50a5770b1fc882acdcbfd0a54e
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-04-03 12:17:15 +0530

    Time: 0 ms (100%), Space: 43.6 MB (86.84%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -8,12 +8,12 @@ class Solution {
             if(nums[mid] == target) return mid;
 
             else if(nums[start] <= nums[mid]){
-                if(target >= nums[start] && target < nums[mid]){
+                if(target >= nums[start] && target <= nums[mid]){
                     end = mid - 1;
                 }else start = mid + 1;
             }
             else{
-                if(target >= nums[mid] && target < nums[end]){
+                if(target >= nums[mid] && target <= nums[end]){
                     start = mid + 1;
                 }else end = mid - 1;
             }
