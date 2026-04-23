58fd406dccd2233ca57112811ad9e1ba2835b5d0
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-04-23 17:31:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      select salary from Employee
+      order by salary desc
+      limit 1
+      offset 1
+
+  );
+END
