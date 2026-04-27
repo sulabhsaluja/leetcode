78951023a2909744af33187dd64fc149e80b2950
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-04-27 16:47:17 +0530

    Time: 18 ms (95.29%), Space: 83.3 MB (8.38%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,22 +1,27 @@
 class Solution {
+    static int[][] dp ;
     public boolean canPartition(int[] nums) {
-        long sum = 0;
-        for(int i = 0 ; i < nums.length; i++){
-            sum+=nums[i];
-        }
+        int n = nums.length;
+        int sum = 0;
+        for(int i = 0 ; i < nums.length ; i++) sum += nums[i];
         if(sum % 2 != 0) return false;
-        return check(nums,nums.length, sum / 2) == 1 ? true : false;
+        int target = sum / 2;
+        dp = new int[n + 1][target + 1];
+        for(int[] row : dp) Arrays.fill(row, -1);
+        return check(nums, n, target) == 1;
     }
-    public int check(int[] nums, int n, long sum){
-        
-        if(sum == 0) return 1;
+
+    public int check(int[] nums, int n, int target){
+        if(target == 0) return 1;
         if(n == 0) return 0;
-
-        if(nums[n - 1] > sum) return check(nums, n - 1, sum);
-
-        if((check(nums, n - 1, sum - nums[n - 1]) == 1 || check(nums, n - 1, sum) == 1)) return 1;
-
-        return 0;
+        if(dp[n][target] != -1) return dp[n][target];
+        if(nums[n - 1] > target){
+            dp[n][target] = check(nums, n - 1, target);
+        }
+        else{
+            if(check(nums, n - 1, target - nums[n - 1]) == 1 || check(nums, n - 1, target) == 1) dp[n][target] = 1;
+            else dp[n][target] = 0;
+        }
+        return dp[n][target];
     }
-
 }
