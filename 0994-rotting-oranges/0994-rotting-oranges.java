d6afe3ee7903cdfcdbcfe3d814f6793716f2cdd4
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-05-14 18:51:19 +0530

    Time: 1 ms (100%), Space: 43.9 MB (91.33%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,49 +1,52 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-
+        int n = grid.length;
+        int m = grid[0].length;
         Queue<int[]> q = new LinkedList<>();
+        int minutes = 0;
         int freshCount = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    q.add(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
-                    freshCount++;
-                }
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 2) q.add(new int[]{i,j});
+                else if(grid[i][j] == 1) freshCount++;
             }
         }
-
         if (freshCount == 0) return 0;
-
-        int[][] directions = {{-1,0}, {0,-1}, {0,1}, {1,0}};
-        int minutes = 0;
-
-        while (!q.isEmpty()) {
-            int N = q.size();
-
-            while (N-- > 0) {
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int x = 0 ; x < size ; x++){
                 int[] curr = q.poll();
                 int i = curr[0];
                 int j = curr[1];
 
-                for (int[] dir : directions) {
-                    int new_i = i + dir[0];
-                    int new_j = j + dir[1];
-
-                    if (new_i >= 0 && new_i < m && new_j >= 0 && new_j < n && grid[new_i][new_j] == 1) {
-                        grid[new_i][new_j] = 2;
-                        freshCount--;
-                        q.add(new int[]{new_i, new_j});
-                    }
+                if(i - 1 >= 0 && grid[i - 1][j] == 1){
+                    grid[i - 1][j] = 2;
+                    freshCount--;
+                    q.add(new int[]{i - 1, j});
+                    
                 }
+                if(j - 1 >= 0 && grid[i][j - 1] == 1){
+                    grid[i][j - 1] = 2;
+                    freshCount--;
+                    q.add(new int[]{i, j - 1});
+                    
+                }
+                if(i + 1 < n && grid[i + 1][j] == 1){
+                    grid[i + 1][j] = 2;
+                    freshCount--;
+                    q.add(new int[]{i + 1, j});
+                    
+                }
+                if(j + 1 < m && grid[i][j + 1] == 1){
+                    grid[i][j + 1] = 2;
+                    freshCount--;
+                    q.add(new int[]{i, j + 1});
+                    
+                }
+                
             }
-
             minutes++;
         }
-
-        return freshCount == 0 ? (minutes - 1) : -1;
+        return freshCount == 0 ? minutes - 1 : -1;
     }
 }
