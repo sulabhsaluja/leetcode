4b7cd7540cda53325f08de9d6fa7d5242aec4e1e
Sulabh Saluja <sulabhsaluja111@gmail.com>
2026-07-07 16:41:14 +0530

    Time: 7 ms (82.94%), Space: 48.3 MB (21.24%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,34 +1,31 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
+    public int minEatingSpeed(int[] arr, int h) {
         int low = 1;
-        int high = findMax(piles);
-        int ans = high;
-        int totalTime = 0;
+        int high = findMax(arr);
+        int ans = 0;
         while(low <= high){
             int mid = low + (high - low) / 2;
-            totalTime = reqTime(piles, mid);
-            if(totalTime <= h){
+            long hours = reqTime(arr, mid);
+            if(hours <= h){
                 ans = mid;
                 high = mid - 1;
-            }else low = mid + 1;
+            }
+            else low = mid + 1;
         }
         return ans;
-        
     }
-    public static int findMax(int[] arr){
+    public int findMax(int[] arr){
         int max = Integer.MIN_VALUE;
-        for(int i = 0 ; i < arr.length ; i++){
-            if(arr[i] > max){
-                max = arr[i];
-            }
+        for(int x : arr){
+            max = Math.max(max, x);
         }
         return max;
     }
-    public static int reqTime(int[] arr, int hourly){
-        int Totalhr = 0;
-        for(int i = 0 ; i < arr.length ; i++){
-            Totalhr += (int)Math.ceil((double)arr[i] /hourly);
+    public long reqTime(int[] arr, int hourly){
+        long total = 0;
+        for(int x : arr){
+            total += (x + hourly - 1) / hourly;
         }
-        return Totalhr;
+        return total;
     }
 }
